d90689af48cecf3c10a3bbb091c3888cd1a42771
PramestiDyah67 <62537342+PramestiDyah67@users.noreply.github.com>
2020-05-04 16:00:28 +0700

    Initial commit

---
+++ DBBiodata.java
@@ -0,0 +1,66 @@
+package id.d3ti.oopl.ketujuh.jdbc;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.mysql.jdbc.*;
+
+public class DBBiodata {
+Connection con;
+Statement st;
+ResultSet rs;
+	public DBBiodata() {
+		try 
+		{
+			/*Class.forName("com.mysql.jdbc.Driver");
+			con = (Connection)
+			DriverManager.getConnection("jdbc:mysql://127.0.0.1/biodata?user=root&password=");**/
+			String url ="jdbc:mysql://localhost:3306/biodata";
+            String user="root";
+            String pass="";
+            Class.forName("com.mysql.jdbc.Driver");
+            con =(Connection) DriverManager.getConnection(url,user,pass);
+            st = (Statement) con.createStatement();
+            System.out.println("koneksi berhasil;");
+				Class.forName("com.mysql.jdbc.Driver");
+				 con =(Connection) DriverManager.getConnection(url,user,pass);
+				 st = (Statement) con.createStatement();
+				 System.out.println("koneksi berhasil;");
+			}catch (SQLException e) {
+				// TODO Auto-generated catch blocks
+				e.printStackTrace();
+				System.err.println("koneksi gagal" +e.getMessage());
+			}catch (ClassNotFoundException e) {
+				e.printStackTrace();
+		}
+	}
+	public Vector data() {
+		try {
+			st = (Statement) con.createStatement();
+			rs = (ResultSet) st.executeQuery ("SELECT * FROM mahasiswa");
+		} catch (SQLException e){
+			e.printStackTrace();
+		}
+		Vector dBdata = new Vector(1);
+		try {
+			while(rs.next()) {
+				Vector rows = new Vector();
+				rows.add(rs.getString(1));
+				rows.add(rs.getString(2));
+				rows.add(rs.getString(3));
+				rows.add(rs.getString(4));
+				rows.add(rs.getString(5));
+				rows.add(rs.getString(6));
+				dBdata.addElement(rows);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return dBdata;
+	}
+}
---
+++ JFrame.java
@@ -0,0 +1,62 @@
+package id.d3ti.oopl.ketujuh.jdbc;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.util.Vector;
+
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+public class JFrame extends javax.swing.JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	DBBiodata biodata; 
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame frame = new JFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		biodata = new DBBiodata();
+		Vector columnNames = new Vector();
+		columnNames.addElement("NIM");
+		columnNames.addElement("Nama");
+		columnNames.addElement("Jenis Kelamin");
+		columnNames.addElement("Tempat Lahir");
+		columnNames.addElement("Tanggal Lahir");
+		columnNames.addElement("Alamat");
+		table = new JTable();
+		DefaultTableModel dtm = new DefaultTableModel(biodata.data(), columnNames);
+		table.setModel(dtm);
+		scrollPane.setColumnHeaderView(table);
+	}
+
+}
